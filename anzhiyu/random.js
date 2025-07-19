3fef88a4d855acb9e6121538483044869b9a1b93
诸荣博 <1737021069@qq.com>
2025-07-19 10:19:31 +0800

    Site updated: 2025-07-19 10:19:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/14/Hexo博客维护/","2025/07/10/hello-world/","2025/07/15/使用Github和PicGO来搭建图床/","2025/07/19/记一次奇怪的Mysql异常/","2025/07/11/我的第一篇博客文章/","2025/07/13/语雀文本编辑器试用/"];function toRandomPost(){
+var posts=["2025/07/10/hello-world/","2025/07/14/Hexo博客维护/","2025/07/11/我的第一篇博客文章/","2025/07/13/语雀文本编辑器试用/","2025/07/15/使用Github和PicGO来搭建图床/","2025/07/19/记一次奇怪的Mysql异常/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
